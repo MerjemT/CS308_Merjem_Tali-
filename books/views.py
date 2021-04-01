3faa552be4eea1cdd2cbfd4e878aee1a665d24a8
MerjemT <73403322+MerjemT@users.noreply.github.com>
2021-04-01 04:09:17 +0200

    Add files via upload

---
+++ books/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/book_list.html'
+
+    @api_view(['GET', 'POST'])
+    def snippet_list(request):
+        """
+        List all code snippets, or create a new snippet.
+        """
+        if request.method == 'GET':
+            snippets = Snippet.objects.all()
+            serializer = SnippetSerializer(snippets, many=True)
+            return Response(serializer.data)
+
+        elif request.method == 'POST':
+            serializer = SnippetSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookDetailsView(DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = 'books/book_detail.html'
+
+
+def snippet_detail(request, pk):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
