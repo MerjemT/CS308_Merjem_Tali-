c66e5803188cb99fcff62ea20e00c47a74720aeb
MerjemT <73403322+MerjemT@users.noreply.github.com>
2021-05-22 18:33:30 +0200

    Update models.py

--- books/models.py
+++ books/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Author(models.Model):
+  name = models.CharField(max_length=200)
+  added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  created_date = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return self.name
 
 class Book(models.Model):
 
