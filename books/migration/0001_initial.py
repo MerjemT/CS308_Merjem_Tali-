bf2d321cb05a6cf02306229a4aedb72624dc99de
MerjemT <73403322+MerjemT@users.noreply.github.com>
2021-05-22 18:49:46 +0200

    Add files via upload

---
+++ books/migration/0001_initial.py
@@ -0,0 +1,32 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('username', models.CharField(max_length=25)),
+                ('name', models.CharField(max_length=25)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=25)),
+                ('publisher', models.CharField(max_length=25)),
+                ('publication_date', models.DateTimeField(verbose_name='publication date')),
+                ('number_of_pages', models.IntegerField()),
+                ('authors', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='books.author')),
+            ],
+        ),
+    ]
