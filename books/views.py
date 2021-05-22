9d8ff0c49a4b4176976bfcbaace1ed11a9544971
MerjemT <73403322+MerjemT@users.noreply.github.com>
2021-05-22 18:28:57 +0200

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,54 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Book
+from .models import Book, Author
+from . import serializers
+from rest_framework import generics
 
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = serializers.BookSerializer
 
-class BookListView(ListView):
-    model = Book
-    context_object_name = 'book_list'
-    template_name = 'books/book_list.html'
+    class BookDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = serializers.BookSerializer
+    lookup_url_kwarg = 'id'
 
-    @api_view(['GET', 'POST'])
-    def snippet_list(request):
-        """
-        List all code snippets, or create a new snippet.
-        """
-        if request.method == 'GET':
-            snippets = Snippet.objects.all()
-            serializer = SnippetSerializer(snippets, many=True)
-            return Response(serializer.data)
+    class AuthorListView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = serializers.AuthorSerializer
 
-        elif request.method == 'POST':
-            serializer = SnippetSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(username=self.request.user)
 
-
-class BookDetailsView(DetailView):
-    model = Book
-    context_object_name = 'book'
-    template_name = 'books/book_detail.html'
-
-
-def snippet_detail(request, pk):
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    class AuthorDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = serializers.AuthorSerializer
+    lookup_url_kwarg = 'id'
