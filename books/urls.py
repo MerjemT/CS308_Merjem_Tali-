57e1ae3e31789e3bfd9c65871718e880e26e1115
MerjemT <73403322+MerjemT@users.noreply.github.com>
2021-05-22 20:48:39 +0200

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
     path('authors/', views.AuthorListView.as_view(), name="authors"),
     path('author/<int:id>/', views.AuthorDetailsView.as_view(), name="author")
+    path('getbooks', views.get_books),
+    path('addbook', views.add_book),
+    path('updatebook/<int:book_id>', views.update_book),
+    path('deletebook/<int:book_id>', views.delete_book)
+
 ]
 
