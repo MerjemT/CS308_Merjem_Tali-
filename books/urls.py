7a2ff07099834945f6afc3445bd5a54bcf7d353f
MerjemT <73403322+MerjemT@users.noreply.github.com>
2021-05-22 18:30:28 +0200

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -4,5 +4,7 @@ from .views import BookListView, BookDetailView
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
-
+    path('authors/', views.AuthorListView.as_view(), name="authors"),
+    path('author/<int:id>/', views.AuthorDetailsView.as_view(), name="author")
 ]
+
